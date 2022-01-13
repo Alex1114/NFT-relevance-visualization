051794184b9a4cad1a18dd917b2043df5446e041
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2022-01-13 16:56:19 +0800

    NFT

---
+++ src/projectList.js
@@ -0,0 +1,28 @@
+module.exports = [
+    //{contract:'0xa3aee8bce55beea1951ef834b99f3ac60d1abeeb', name: 'VeeFriends'},
+    // {contract:'0xf3e778f839934fc819cfa1040aabacecba01e049', name: 'Avastars'},
+    {contract:'0x1EdD2Ddfc9F81e55aB1Ec5431a77AF7d1681c725', name: 'KNS'},
+    {contract:'0x42f1654b8eeb80c96471451b1106b63d0b1a9fe1', name: 'Chubbiverse'},
+    {contract:'0xBd3531dA5CF5857e7CfAA92426877b022e612cf8', name: 'PudgyPenguins'},
+    {contract:'0x3bf2922f4520a8BA0c2eFC3D2a1539678DaD5e9D', name: '0N1'},
+
+    {contract:'0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d', name: 'BAYC'},
+    // {contract:'0xba30e5f9bb24caa003e9f2f0497ad287fdf95623', name: 'BAKC'},
+    {contract:'0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7', name: 'Meebits'},
+    //{contract:'0xbCCaa7ACb552A2c7eb27C7eb77c2CC99580735b9', name: 'Artvatars'}, (Polygon)
+    // {contract:'0xf78296dfcf01a2612c2c847f68ad925801eeed80', name: 'Pickle'},
+    // {contract:'0x85f740958906b317de6ed79663012859067e745b', name: 'Craniums'},
+    // {contract:'0x4581649af66bccaee81eebae3ddc0511fe4c5312', name: 'Alien Boy'},
+    // {contract:'0x74a69df3adc7235392374f728601e49807de4b30', name: 'Misfit U'},
+    //'0xedb61f74b0d09b2558f1eeb79b247c1f363ae452', // Gutter cats -- non-standard contract (ERC-165)
+    // {contract:'0x3f0785095a660fee131eebcd5aa243e529c21786', name: 'Super Yeti'},
+    //'0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb', // punks -- non-standard contract (ERC-20)
+    // {contract:'0x31385d3520bced94f77aae104b406994d8f2168c', name: 'B-gan Punks'},
+    // {contract:'0x7c40c393dc0f283f318791d746d894ddd3693572', name: 'Wrapped Mooncats'},
+    // {contract:'0xc2c747e0f7004f9e8817db2ca4997657a7746928', name: 'Hashmasks'},
+ 
+    // {contract:'0xd95B7cCe83d4abDC006C991E9cE0542d30E40D49', name: 'MetaChamps'},
+    // {contract:'0xc9E3Ca32CAaA6ee67476C5d35d4B8ec64F58D4Ad', name: 'RugPullFrens'},
+    // {contract:'0x29c4619133827C1D5f37EDa348127244390E6569', name: 'Moonwalkers'},
+    // {contract:'0xc9e2c9718fF7d3129B9Ac12168195507e4275Cea', name: '0xVampire'},
+]
